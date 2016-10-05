ca288cd0f97232ec6d4c425c2ddd27cc78a00bd0
Marco Pizarro-Silva <marcopizarro@users.noreply.github.com>
2016-10-04 19:47:20 -0700

    Updated ZStabalization

--- ZstabalizationTest1/ZstabalizationTest1.ino
+++ ZstabalizationTest1/ZstabalizationTest1.ino
@@ -5,6 +5,7 @@ MS5837 sensor;
 
 void setup() {
   
+  //pinMode(##buttonPin##, INPUT);
   Serial.begin(9600);
   
   Serial.println("Starting");
@@ -13,11 +14,13 @@ void setup() {
 
   sensor.init();
   
-  sensor.setFluidDensity(997); // kg/m^3 (997 freshwater, 1029 for seawater)
+  sensor.setFluidDensity(1001); // kg/m^3 (997 freshwater, 1029 for seawater)
 }
 
 void loop() {
 
+  //float depthSavedState = 0;
+  //bool lockedZ = false;
   sensor.read();
 
   Serial.print("Pressure: "); 
@@ -30,14 +33,39 @@ void loop() {
   
   Serial.print("Depth: "); 
   Serial.print(sensor.depth()); 
-  Serial.println(" m");
+  Serial.println(" m deep");
   
   Serial.print("Altitude: "); 
   Serial.print(sensor.altitude()); 
   Serial.println(" m above mean sea level");
 
+  /*
+  buttonState = digitalRead(##buttonPin##);
+  // if putton pressed, ACTIVATE Z
+  if(buttonState == HIGH && !lockedZ){
+    depthSavedState = sensor.depth();
+    lockedZ = true;
+  }
+  // if putton pressed, DEACTIVATE Z
+  if(buttonState == HIGH && lockedZ){
+    depthSavedState = sensor.depth();
+    lockedZ = false;
+  }
+    // if Z STABLAIZATION = ACTIVE
+    while(lockedZ){
+    // if ROV is above saved state
+      if(sensor.depth() > depthSavedState){
+          // Move ROV DOWN
+      }
+    // if ROV is below saved state
+      if(sensor.depth() < depthSavedState){
+          // Move ROV UP
+      }
+      
+    }
+    
+
+*/
+  
   delay(1000);
 }
-
-
-
