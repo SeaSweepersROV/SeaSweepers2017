2562ec49016d9ea00b5cef98198182e88189a0a9
Michael Georgariou III <michael@seasweepersrov.com>
2017-01-04 17:47:12 -0800

    Added a RGBW LED test

---
+++ simple_rgbw_test/simple_rgbw_test.ino
@@ -0,0 +1,26 @@
+#include <Adafruit_NeoPixel.h>
+
+#define PIN            6
+#define NUMPIXELS      7
+
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRBW + NEO_KHZ800);
+
+int delayval = 500; // delay for half a second
+
+void setup() {
+  pixels.begin(); // This initializes the NeoPixel library.
+}
+
+void loop() {
+  // For a set of NeoPixels the first NeoPixel is 0, second is 1, all the way up to the count of pixels minus one.
+  for(int i=0;i<NUMPIXELS;i++){
+    pixels.setPixelColor(i, pixels.Color(255,0,0,0));
+    pixels.show();
+  }
+    delay(delayval);
+  for(int i=0;i<NUMPIXELS;i++){
+    pixels.setPixelColor(i, pixels.Color(0,0,0,255));
+    pixels.show();
+  }
+    delay(delayval);
+}
