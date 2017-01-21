b34b172665eba863b7d7edfce20227c2e32c7508
Michael Georgariou III <michael@seasweepersrov.com>
2017-01-21 09:10:19 -0800

    now flips between red and white with a switch

--- simple_rgbw_test/simple_rgbw_test.ino
+++ simple_rgbw_test/simple_rgbw_test.ino
@@ -2,25 +2,41 @@
 
 #define PIN            6
 #define NUMPIXELS      7
-
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRBW + NEO_KHZ800);
 
 int delayval = 500; // delay for half a second
 
 void setup() {
   pixels.begin(); // This initializes the NeoPixel library.
+  pinMode(3, INPUT);
+  pinMode(4,OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
   // For a set of NeoPixels the first NeoPixel is 0, second is 1, all the way up to the count of pixels minus one.
-  for(int i=0;i<NUMPIXELS;i++){
-    pixels.setPixelColor(i, pixels.Color(0,0,0,255));
-    pixels.show();
+  digitalWrite(4, HIGH);
+  if (digitalRead(3) == HIGH) {
+      for(int i=0;i<NUMPIXELS;i++){
+        pixels.setPixelColor(i, pixels.Color(0,0,0,255));
+        pixels.show();
+      }
+        Serial.println("ONE");
   }
-    delay(delayval);
-  for(int i=0;i<NUMPIXELS;i++){
-    pixels.setPixelColor(i, pixels.Color(255,255,255,0));
-    pixels.show();
+  if (digitalRead(3) == LOW) {
+      for(int i=0;i<NUMPIXELS;i++){
+        pixels.setPixelColor(i, pixels.Color(255,0,0,0));
+        pixels.show();
+      }
+        Serial.println("TWO");
   }
-    delay(delayval);
+  else {
+    Serial.println("GAY");
+      for(int i=0;i<NUMPIXELS;i++){
+        pixels.setPixelColor(i, pixels.Color(0,0,255,0));
+        pixels.show();
+      }
+    
+  }
+  delay(10);
 }
