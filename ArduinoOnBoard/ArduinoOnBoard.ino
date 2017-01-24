610feca557f3a7a4efd2549f9a3c0859123ef105
Marco Pizarro-Silva <marcopizarro@users.noreply.github.com>
2017-01-23 19:41:13 -0800

    Remove Unnecessary Comments

--- ArduinoOnBoard/ArduinoOnBoard.ino
+++ ArduinoOnBoard/ArduinoOnBoard.ino
@@ -28,6 +28,7 @@ THE SOFTWARE.
 #include <Adafruit_Sensor.h>
 #include <Adafruit_LSM303_U.h>
 #include <Adafruit_Simple_AHRS.h>
+#include "DHT.h"
 
 MS5837 sensor;
 
@@ -41,26 +42,11 @@ Adafruit_Simple_AHRS          ahrs(&accel, &mag);
 // Example testing sketch for various DHT humidity/temperature sensors
 // Written by ladyada, public domain
 
-#include "DHT.h"
+
 
 #define DHTPIN 2     // what digital pin we're connected to
-
-// Uncomment whatever type you're using!
 #define DHTTYPE DHT11   // DHT 11
-//#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
-//#define DHTTYPE DHT21   // DHT 21 (AM2301)
 
-// Connect pin 1 (on the left) of the sensor to +5V
-// NOTE: If using a board with 3.3V logic like an Arduino Due connect pin 1
-// to 3.3V instead of 5V!
-// Connect pin 2 of the sensor to whatever your DHTPIN is
-// Connect pin 4 (on the right) of the sensor to GROUND
-// Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
-
-// Initialize DHT sensor.
-// Note that older versions of this library took an optional third parameter to
-// tweak the timings for faster processors.  This parameter is no longer needed
-// as the current DHT reading algorithm adjusts itself to work on faster procs.
 DHT dht(DHTPIN, DHTTYPE);
 
 void setup()
