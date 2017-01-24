a1e5bdd2af009b45e3c5964ca909a06ef4b4cbe7
Marco Pizarro-Silva <marcopizarro@users.noreply.github.com>
2017-01-23 19:39:22 -0800

    Updatesz to Arduino Code and Schematic

--- ArduinoOnBoard/ArduinoOnBoard.ino
+++ ArduinoOnBoard/ArduinoOnBoard.ino
@@ -38,6 +38,31 @@ Adafruit_LSM303_Mag_Unified   mag(30302);
 // Create simple AHRS algorithm using the above sensors.
 Adafruit_Simple_AHRS          ahrs(&accel, &mag);
 
+// Example testing sketch for various DHT humidity/temperature sensors
+// Written by ladyada, public domain
+
+#include "DHT.h"
+
+#define DHTPIN 2     // what digital pin we're connected to
+
+// Uncomment whatever type you're using!
+#define DHTTYPE DHT11   // DHT 11
+//#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
+//#define DHTTYPE DHT21   // DHT 21 (AM2301)
+
+// Connect pin 1 (on the left) of the sensor to +5V
+// NOTE: If using a board with 3.3V logic like an Arduino Due connect pin 1
+// to 3.3V instead of 5V!
+// Connect pin 2 of the sensor to whatever your DHTPIN is
+// Connect pin 4 (on the right) of the sensor to GROUND
+// Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
+
+// Initialize DHT sensor.
+// Note that older versions of this library took an optional third parameter to
+// tweak the timings for faster processors.  This parameter is no longer needed
+// as the current DHT reading algorithm adjusts itself to work on faster procs.
+DHT dht(DHTPIN, DHTTYPE);
+
 void setup()
 {
   Serial.begin(9600);
@@ -49,7 +74,10 @@ void setup()
           delay(500);  
      Serial.println(".");   
            delay(500); 
-      
+
+
+    dht.begin();
+  
   // Initialize the sensors.
   accel.begin();
   mag.begin();
@@ -65,7 +93,23 @@ void loop()
   // Use the simple AHRS function to get the current orientation.
   if (ahrs.getOrientation(&orientation))
   {
-    /* 'orientation' should have valid .roll and .pitch fields */
+
+    
+   sensor.read();
+
+  Serial.print("Pressure: "); 
+  Serial.print(sensor.pressure()); 
+  Serial.println(" mbar");
+  
+  Serial.print("Onboard Temperature: "); 
+  Serial.print(sensor.temperature()); 
+  Serial.println(" deg C");
+  
+  Serial.print("Depth: "); 
+  Serial.print(sensor.depth() + 0.16); 
+  Serial.println(" m");
+
+      /* 'orientation' should have valid .roll and .pitch fields */
     Serial.print(F("Orientation: "));
     Serial.print(orientation.roll);
     Serial.print(F(" "));
@@ -73,20 +117,35 @@ void loop()
     Serial.print(F(" "));
     Serial.print(orientation.heading);
     Serial.println(F(""));
-    
-      sensor.read();
 
-  Serial.print("Pressure: "); 
-  Serial.print(sensor.pressure()); 
-  Serial.println(" mbar");
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  float f = dht.readTemperature(true);
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
   
-  Serial.print("Temperature: "); 
-  Serial.print(sensor.temperature()); 
-  Serial.println(" deg C");
+  // Compute heat index in Fahrenheit (the default)
+  float hif = dht.computeHeatIndex(f, h);
+  // Compute heat index in Celsius (isFahreheit = false)
+  float hic = dht.computeHeatIndex(t, h, false);
   
-  Serial.print("Depth: "); 
-  Serial.print(sensor.depth()); 
-  Serial.println(" m");
+     Serial.print("Humidity: ");
+     Serial.print(h);
+     Serial.print(" %\t");
+     Serial.print("Temperature: ");
+     Serial.print(t);
+     Serial.print(" *C ");
+     Serial.print(f);
+     Serial.println(" *F\t");
+    
   }
   
   delay(1000);
