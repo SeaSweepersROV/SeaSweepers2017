b05cb5086c3be8622c18b0b4b625ee74b34c6c47
Michael Georgariou III <michael@seasweepersrov.com>
2017-04-15 09:39:33 -0700

    Added Official Motor Code (Tentative)
    hope this works :)

---
+++ MotorsRX/MotorsRX.ino
@@ -0,0 +1,278 @@
+/*
+  Receiving Motor Code
+  Adds mutliaxis support in order to make JP happy
+
+  Written for the Sea Sweepers ROV Club
+  Michael Georgariou 2017
+*/
+
+#include <Servo.h>
+Servo H1;
+Servo H2;
+Servo H3;
+Servo H4;
+Servo V1;
+Servo V2;
+Servo V3;
+Servo V4;
+
+unsigned char val[16];
+int Joystick1A; //vertical
+  int Map1A;
+int Joystick1B;
+  int Map1B;
+int Joystick1C;
+  int Map1C;
+int Joystick2A; //horizontal
+  int Map2A;
+int Joystick2B;
+  int Map2B;
+int Joystick2C;
+  int Map2C;
+
+unsigned char checksum0;
+unsigned char checksum1;
+unsigned char checksum2;
+unsigned char checksum3;
+unsigned char checksum4;
+unsigned char checksum5;
+unsigned char checksum6;
+unsigned char checksum7;
+unsigned char FirstByte;
+unsigned char LastByte;
+
+unsigned char checkit0;
+unsigned char checkit1;
+unsigned char checkit2;
+unsigned char checkit3;
+unsigned char checkit4;
+unsigned char checkit5;
+unsigned char checkit6;
+unsigned char checkit7;
+  
+
+int VertVar; //to store number for stabilization
+
+int Crab;
+int Pitch;
+
+void setup() {
+
+  Serial1.begin(115200);
+
+  H1.attach(2); // left front
+  H2.attach(3); // right front
+  H3.attach(4); // right back
+  H4.attach(5); // left back
+  V1.attach(6);
+  V2.attach(7);
+  V3.attach(8);
+  V4.attach(9);
+}
+
+void loop() {
+
+  Communication();
+  MapJoysticks();
+  MotorWrite();
+  DrivingLight();
+  FunLEDs();
+
+//  if () { //ADD SWTICH
+//    VertStabilization(); //stays completely flat (always on)
+//  }
+}
+
+void Communication() {
+  Serial1.write(1);
+  Serial1.write(2);
+  Serial1.write(3);
+
+  if (Serial1.available()>19) { 
+    FirstByte=Serial1.read();
+
+    if (FirstByte == 1) {
+      val[0]=Serial1.read();         // least significant 8 bits
+      val[1]=Serial1.read();         // most significant 2 bits   
+       checksum0=Serial1.read();     // checksum
+  
+      val[2]=Serial1.read();
+      val[3]=Serial1.read();
+       checksum1=Serial1.read();
+  
+      val[4]=Serial1.read();
+      val[5]=Serial1.read(); 
+       checksum2=Serial1.read();
+  
+      val[6]=Serial1.read();         // least significant 8 bits
+      val[7]=Serial1.read();         // most significant 2 bits   
+       checksum3=Serial1.read();     // checksum
+  
+      val[8]=Serial1.read();
+      val[9]=Serial1.read();
+       checksum4=Serial1.read();
+  
+      val[10]=Serial1.read();
+      val[11]=Serial1.read(); 
+       checksum5=Serial1.read();
+
+      LastByte = Serial1.read();
+      
+    }
+  //See if Checksums equal zero
+  CombineValues();
+  }
+}
+
+void MotorWrite() { // Writes joystick values to ESCs
+  
+  MapJoysticks();
+  
+  
+    if ((Map2C < 1600) && (Map2C > 1400)) { //if Map2C is neutral
+    H2.write(Map2A);
+    H3.write(Map2A);
+    H1.write(Map2B);
+    H4.write(Map2B);
+  }
+  
+  if (Map2C > 1600) {
+    Crab = map(Map2C, 1600, 1900, 0, 400);
+    if ((Map2A - Crab) > 1100){
+      H2.write(Map2A - Crab);
+      H3.write(Map2A - Crab);
+    }
+    else {
+      H2.write(Map2A);
+      H3.write(Map2A);
+    }
+    if ((Map2B + Crab) < 1900) {
+      H1.write(Map2B + Crab);
+      H4.write(Map2B + Crab);
+    }
+    else {
+      H1.write(Map2B);
+      H4.write(Map2B);
+    }
+  }
+    
+  if (Map2C < 1400) {
+    Crab = map(Map2C, 1400, 1100, 0, 400);
+    if ((Map2A - Crab) > 1100){
+      H2.write(Map2A - Crab);
+      H3.write(Map2A - Crab);
+    }
+    else {
+      H2.write(Map2A);
+      H3.write(Map2A);
+    }
+    if ((Map2B + Crab) < 1900) {
+      H1.write(Map2B + Crab);
+      H4.write(Map2B + Crab);
+    }
+    else {
+      H1.write(Map2B);
+      H4.write(Map2B);
+    }
+  }
+  
+  
+  if ((Map1C < 1600) && (Map1C > 1400)) { //if Map2C is neutral
+    V2.write(Map1A);
+    V3.write(Map1A);
+    V1.write(Map1B);
+    V4.write(Map1B);
+  }
+  if (Map1C > 1600) {
+    Pitch = map(Map1C, 1600, 1900, 0, 400);
+    if ((Map2A + Pitch) < 1900){
+      H1.write(Map2A + Pitch);
+      H2.write(Map2A + Pitch);
+    }
+    else {
+      H1.write(Map2A);
+      H2.write(Map2A);
+    }
+    if ((Map2B - Pitch) > 1100) {
+      H3.write(Map2B - Pitch);
+      H4.write(Map2B - Pitch);
+    }
+    else {
+      H3.write(Map2B);
+      H4.write(Map2B);
+    }
+  }
+    
+  if (Map2C < 1400) {
+    Pitch = map(Map2C, 1400, 1100, 0, 400);
+    if ((Map2A - Pitch) > 1100){
+      H1.write(Map2A - Pitch);
+      H2.write(Map2A - Pitch);
+    }
+    else {
+      H1.write(Map2A);
+      H2.write(Map2A);
+    }
+    if ((Map2B + Pitch) < 1900) {
+      H3.write(Map2B + Pitch);
+      H4.write(Map2B + Pitch);
+    }
+    else {
+      H3.write(Map2B);
+      H4.write(Map2B);
+    }
+  }
+}
+
+void CombineValues() {
+  // Add up checksums
+  checkit0 = val[0]+val[1]+checksum0;
+  checkit1 = val[2]+val[3]+checksum1;
+  checkit2 = val[4]+val[5]+checksum2; 
+  checkit3 = val[6]+val[7]+checksum3;
+  checkit4 = val[8]+val[9]+checksum4;
+  checkit5 = val[10]+val[11]+checksum5; 
+  //check if they equal zero
+  if (checkit0==0)  {
+    Joystick1A = val[1]<<8 | val[0];
+  }
+  if (checkit1==0)  {
+    Joystick1B = val[3]<<8 | val[2];
+  }
+  if (checkit2==0)  {
+    Joystick1C = val[5]<<8 | val[4]; 
+  }
+  if (checkit3==0)  {
+    Joystick2A = val[7]<<8 | val[6]; 
+  }
+  if (checkit4==0)  {
+    Joystick2B = val[9]<<8 | val[8]; 
+  }
+  if (checkit5==0)  {
+    Joystick2C = val[11]<<8 | val[10]; 
+  }
+}
+
+void MapJoysticks() { // Maps joysticks for use with ESC
+  Map1A = map(Joystick1A, 0, 1023, 1100, 1900);
+  Map1B = map(Joystick1B, 0, 1023, 1100, 1900);
+  Map1C = map(Joystick1C, 0, 1023, 1100, 1900); 
+  Map2A = map(Joystick2A, 0, 1023, 1100, 1900);
+  Map2B = map(Joystick2B, 0, 1023, 1100, 1900);
+  Map2C = map(Joystick2C, 0, 1023, 1100, 1900);
+}
+
+void DrivingLight() {
+  
+}
+
+void FunLEDs() {
+  
+}
+
+void VertStabilization() {
+  
+
+  
+}
+
---
+++ MotorsTX/MotorsTX.ino
@@ -0,0 +1,133 @@
+/*
+  Transmitting Motor Code
+  Takes numbers from joysticks and sends them through the Serial1 Port
+  It's alright I guess
+
+  Written for the Sea Sweepers ROV Club
+  Michael Georgariou 2017
+*/
+
+int data [16];    // to send bytes
+int start [2];
+
+int Pin1A = A0;           
+int Joystick1A;   // crab
+
+int Pin1B = A1;
+int Joystick1B;   // up down numbers are backwards
+
+int Pin1C = A2;
+int Joystick1C;   // pitch
+
+int Pin2A = A3;          
+int Joystick2A;   // up down broken
+
+int Pin2B = A4;   // yaw r-l
+int Joystick2B;
+
+int Pin2C = A5;
+int Joystick2C;   // forward-back   fdsafdsaf
+
+unsigned char checksum0;
+unsigned char checksum1;
+unsigned char checksum2;
+unsigned char checksum3;
+unsigned char checksum4;
+unsigned char checksum5;
+unsigned char checksum6;
+unsigned char checksum7;
+unsigned char handshake;
+unsigned char handshake2;
+unsigned char handshake3;
+unsigned char handshake4;
+unsigned char handshake5;
+unsigned char handshake6;
+
+void setup() {
+  Serial.begin(115200);
+  Serial1.begin(115200);
+
+}
+
+void loop() {
+  ReadandMap();
+  Checksums();
+  Communication();
+  delay(20);
+}
+
+void ReadandMap () {
+  Joystick1A = analogRead(Pin1A);
+  data[0] = Joystick1A & 0xFF;
+  data[1] = (Joystick1A >> 8);
+  
+  Joystick1B = analogRead(Pin1B);
+  data[2] = Joystick1B & 0xFF;
+  data[3] = (Joystick1B >> 8);
+  
+  Joystick1C = analogRead(Pin1C);
+  data[4] = Joystick1C & 0xFF;
+  data[5] = (Joystick1C >> 8);
+  
+  Joystick2A = analogRead(Pin2A);
+  data[6] = Joystick2A & 0xFF;
+  data[7] = (Joystick2A >> 8);
+  
+  Joystick2B = analogRead(Pin2B);
+  data[8] = Joystick2B & 0xFF;
+  data[9] = (Joystick2B >> 8);
+
+  Joystick2C = analogRead(Pin2C);
+  data[10] = Joystick2C & 0xFF;      //least significant 8 bit byte
+  data[11] = (Joystick2C >> 8);
+}
+
+void Checksums() {
+  checksum0 = ~(data[0] + data[1]) + 1;
+  checksum1 = ~(data[2] + data[3]) + 1;
+  checksum2 = ~(data[4] + data[5]) + 1;
+  checksum3 = ~(data[6] + data[7]) + 1;
+  checksum4 = ~(data[8] + data[9]) + 1;
+  checksum5 = ~(data[10] + data[11]) + 1;
+}
+
+void Communication() {
+  while (Serial1.available() < 3) {
+    ; //wait for request from receiver
+  }
+  
+  handshake = Serial1.read();
+  handshake2 = Serial1.read();
+  handshake3 = Serial1.read();
+
+  if ((handshake == 1) && (handshake2 == 2) && (handshake3 == 3)) {
+    Serial1.write(1);
+    Serial1.write(data[0]);
+    Serial1.write(data[1]);
+    Serial1.write(checksum0);
+
+    Serial1.write(data[2]);
+    Serial1.write(data[3]); //bytes sent
+    Serial1.write(checksum1);
+
+    Serial1.write(data[4]);
+    Serial1.write(data[5]);
+    Serial1.write(checksum2);
+
+    Serial1.write(data[6]);
+    Serial1.write(data[7]);
+    Serial1.write(checksum3);
+
+    Serial1.write(data[8]);
+    Serial1.write(data[9]); //bytes sent
+    Serial1.write(checksum4);
+
+    Serial1.write(data[10]);
+    Serial1.write(data[11]);
+    Serial1.write(checksum5);
+
+    Serial1.write(2);
+
+  }
+}
+
